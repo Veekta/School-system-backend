e262cfd0c47e3df8b51f32ab2175d5385f749c9d
CallmeVeeTech <vsunday.1710@gmail.com>
2022-07-09 19:52:16 +0100

    files

--- utils/db.js
+++ utils/db.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const url = process.env.URL;
+const localURL = "mongodb://localhost/MySchoolDatabase";
+// const url = process.env.URL;
 
 mongoose
-  .connect(url)
+  .connect(localURL)
   .then(() => {
     console.log("database connected...");
   })
